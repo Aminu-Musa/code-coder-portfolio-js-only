8f0d78be5e5593059cb0521d7930614c871def7a
Aminu-Musa <aminumusa669@gmail.com>
2025-03-18 15:38:50 +0100

    Portfolio design Intiated

---
+++ script.js
@@ -0,0 +1,10 @@
+// Typed.js animated text
+
+var typed = new Typed('#animatedText', {
+    strings: ['Web Developer', 'Geospatial Analyst','Data Scientist'],
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 500,
+    startDelay: 1000,
+    loop: true,
+  });
