bfbc8bb4cb09737c4e8c012b0b99956b2aec80dc
Aminu-Musa <aminumusa669@gmail.com>
2025-03-18 18:07:37 +0100

    Nav Menu Worked on

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 // Typed.js animated text
 
-var typed = new Typed('#animatedText', {
-    strings: ['Web Developer', 'Geospatial Analyst','Data Scientist'],
-    typeSpeed: 60,
-    backSpeed: 40,
-    backDelay: 500,
-    startDelay: 1000,
-    loop: true,
-  });
+var typed = new Typed("#animatedText", {
+  strings: ["Web Developer", "Geospatial Analyst", "Data Scientist"],
+  typeSpeed: 60,
+  backSpeed: 40,
+  backDelay: 500,
+  startDelay: 1000,
+  loop: true,
+});
+
+gsap.from("#gsap-image-animation", { opacity: 0, y: -50, duration: 1.5 });
+
+
+// HANDLING HAMBURGER MENU START
+let navMenuLinks = document.querySelector(".nav-link-menu");
+const navContainer = document.querySelector(".nav-bar-section");
+const closeMenu = document.querySelector(".close-nav-menu-col");
+const openNav = document.querySelector(".open-menu");
+
+navMenuLinks = navMenuLinks.children
+
+openNav.addEventListener("click", () => {
+  navContainer.classList.remove("close-nav");
+});
+
+closeMenu.addEventListener("click", () => {
+  navContainer.classList.add("close-nav");
+});
+
+
+console.log(navMenuLinks[0]);
+
+
+ for (i = 0; i < navMenuLinks.length; i++){
+  navMenuLinks[i].addEventListener("click", ()=>{
+    navContainer.classList.add("close-nav");
+  })}
+
+
+
+
+
+// HANDLING HAMBURGER MENU END
