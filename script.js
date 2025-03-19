b238b3dca53ef42a1cab3404187629a14805cd35
Aminu-Musa <aminumusa669@gmail.com>
2025-03-19 16:04:37 +0100

    logo carosel worked on

--- script.js
+++ script.js
@@ -38,7 +38,22 @@ console.log(navMenuLinks[0]);
   })}
 
 
-
-
-
 // HANDLING HAMBURGER MENU END
+
+
+
+// carosel animation start
+
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+
+
+// carosel animation end
